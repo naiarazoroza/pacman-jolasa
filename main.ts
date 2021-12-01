29110aa4678840bbbd603d9414372a6d59e434f3
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2021-12-01 14:25:11 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let janaria_2: game.LedSprite = null
 let pacman = game.createSprite(2, 4)
 let janaria = game.createSprite(4, 4)
 let mamua = game.createSprite(2, 2)
@@ -11,11 +12,23 @@ basic.forever(function () {
     basic.pause(400)
     if (input.acceleration(Dimension.X) < 200) {
         pacman.move(-1)
-    } else if (input.acceleration(Dimension.X) > -200) {
+    }
+    if (input.acceleration(Dimension.X) > -200) {
         pacman.move(1)
-    } else if (input.acceleration(Dimension.Y) > 200) {
-        pacman.move(1)
-    } else if (input.acceleration(Dimension.Y) < -200) {
-        pacman.move(-1)
+    }
+    if (input.acceleration(Dimension.Y) > 200) {
+        pacman.change(LedSpriteProperty.Y, 1)
+    }
+    if (input.acceleration(Dimension.Y) < -200) {
+        pacman.change(LedSpriteProperty.Y, -1)
+    }
+    if (pacman.isTouching(janaria)) {
+        game.addScore(1)
+        janaria.delete()
+        janaria_2 = game.createSprite(randint(0, 4), randint(0, 4))
+        janaria.set(LedSpriteProperty.Brightness, 10)
+    }
+    if (pacman.isTouching(mamua)) {
+        game.gameOver()
     }
 })
