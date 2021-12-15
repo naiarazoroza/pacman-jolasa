80fb9b62dc2fcb6158071e9f9cb767cf57c5ca3f
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2021-12-15 14:28:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,38 @@
-let janaria_2: game.LedSprite = null
 let pacman = game.createSprite(2, 4)
 let janaria = game.createSprite(4, 4)
 let mamua = game.createSprite(2, 2)
 mamua.set(LedSpriteProperty.Blink, 100)
 mamua.set(LedSpriteProperty.Brightness, 50)
 janaria.set(LedSpriteProperty.Brightness, 10)
-janaria_2.set(LedSpriteProperty.Brightness, 10)
 basic.forever(function () {
-    mamua.move(1)
-    basic.pause(500)
-    mamua.ifOnEdgeBounce()
-    basic.pause(400)
-    if (input.acceleration(Dimension.X) < 200) {
-        pacman.move(-1)
-    }
-    if (input.acceleration(Dimension.X) > -200) {
+    if (input.acceleration(Dimension.X) > 200) {
+        basic.pause(200)
         pacman.move(1)
     }
+    if (input.acceleration(Dimension.X) < -200) {
+        basic.pause(200)
+        pacman.move(-1)
+    }
     if (input.acceleration(Dimension.Y) > 200) {
+        basic.pause(200)
         pacman.change(LedSpriteProperty.Y, 1)
     }
     if (input.acceleration(Dimension.Y) < -200) {
+        basic.pause(200)
         pacman.change(LedSpriteProperty.Y, -1)
     }
     if (pacman.isTouching(janaria)) {
         game.addScore(1)
         janaria.delete()
-        janaria_2 = game.createSprite(randint(0, 4), randint(0, 4))
+        janaria = game.createSprite(randint(0, 4), randint(0, 4))
         janaria.set(LedSpriteProperty.Brightness, 10)
     }
     if (pacman.isTouching(mamua)) {
         game.gameOver()
     }
+})
+basic.forever(function () {
+    basic.pause(500)
+    mamua.move(1)
+    mamua.ifOnEdgeBounce()
 })
