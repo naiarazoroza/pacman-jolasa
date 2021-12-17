6335b8b22eaad0e860d756e959d6b4fd150b8b3e
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2021-12-17 14:23:43 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+game.setLife(3)
 let pacman = game.createSprite(2, 4)
 let janaria = game.createSprite(4, 4)
 let mamua = game.createSprite(2, 2)
@@ -22,13 +23,30 @@ basic.forever(function () {
         pacman.change(LedSpriteProperty.Y, -1)
     }
     if (pacman.isTouching(janaria)) {
+        music.playTone(523, music.beat(BeatFraction.Whole))
         game.addScore(1)
         janaria.delete()
         janaria = game.createSprite(randint(0, 4), randint(0, 4))
         janaria.set(LedSpriteProperty.Brightness, 10)
     }
     if (pacman.isTouching(mamua)) {
-        game.gameOver()
+        music.playTone(131, music.beat(BeatFraction.Whole))
+        game.removeLife(1)
+    }
+    if (game.score() > 3) {
+        basic.pause(300)
+        mamua.move(1)
+        mamua.ifOnEdgeBounce()
+    }
+    if (game.score() > 6) {
+        basic.pause(200)
+        mamua.move(1)
+        mamua.ifOnEdgeBounce()
+    }
+    if (game.score() > 9) {
+        basic.pause(100)
+        mamua.move(1)
+        mamua.ifOnEdgeBounce()
     }
 })
 basic.forever(function () {
